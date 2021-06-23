0c48162d778f75153990169bbd73451a82f865d6
parshantdhall <parshant.dhall@gmail.com>
2021-06-23 16:48:08 +1000

    edited quil editor for links.

--- plugins/wysiwyg/admin/src/components/QuillEditor/index.js
+++ plugins/wysiwyg/admin/src/components/QuillEditor/index.js
@@ -1,6 +1,31 @@
 import React from "react";
-import ReactQuill from "react-quill";
+import ReactQuill, { Quill } from "react-quill";
 import "react-quill/dist/quill.snow.css";
+// ----------------Link Stuff-------------------
+const Link = Quill.import("formats/link");
+class linkType extends Link {
+  static create(value) {
+    // let node = super.create(value);
+    // value = this.sanitize(value);
+    // if (Number(value) !== NaN) {
+    //   node.setAttribute("href", "tel:" + value);
+    //   node.className = "link--tel";
+    // }
+    // if (validations.isEmail(value)) {
+    //   node.setAttribute("href", "mailto:" + value);
+    //   node.className = "link--mail";
+    // }
+    if (value.startsWith("https://") || value.startsWith("http://")) {
+      node.className = "link--external";
+    } else {
+      node.removeAttribute("rel");
+      node.removeAttribute("target");
+    }
+    return node;
+  }
+}
+Quill.register(linkType);
+// -------------End link stuff-----------
 
 const Editor = ({ onChange, name, value }) => {
   const modules = {
